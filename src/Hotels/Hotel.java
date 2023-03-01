36ad8d33368da2417037c2041fbafefdef11d329
IbrahimSaid1 <squ118977@gmail.com>
2023-03-01 16:37:39 +0400

    Crrate table hotel

---
+++ src/Hotels/Hotel.java
@@ -0,0 +1,65 @@
+package Hotels;
+
+import java.sql.*;
+import java.util.Scanner;
+public class Hotel {
+    public static void main(String[] args) {
+        String url = "jdbc:sqlserver://localhost:1433;" +
+                "databaseName=HotelDBMS;" +
+                "encrypt=true;" +
+                "trustServerCertificate=true";
+        String user = "sa";
+        String pass = "root";
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter id: ");
+	Integer id = scanner.nextInt();
+System.out.println("Enter hotel name: ");
+	String name = scanner.next();
+System.out.println("Enter hotel location: ");
+    String location = scanner.next();
+System.out.println("Enter created date: ");  
+	String date = scanner.next();
+System.out.println("Enter updated date: ");  
+	String date1 = scanner.next();
+System.out.println("is_Active: ");  
+	boolean active = scanner.next() != null;
+
+    
+ Connection con = null;
+        try {
+Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+DriverManager.registerDriver(driver);
+            con = DriverManager.getConnection(url, user, pass);
+ Statement st = con.createStatement();
+ 
+ String sql = "CREATE TABLE Hotels ("
+ 		+ "id INT PRIMARY KEY IDENTITY, " +
+		"hotel_name VARCHAR(255) NOT NULL, " +
+		"hotel_location VARCHAR(255), " +
+		"created_date DATE NOT NULL, " +
+		"updated_date DATE, " +
+		"is_Active BIT NOT NULL)";
+ 
+            String sql1 = "insert into Hotels values('" + id
++ "'," + name + ",'" + location + ",'" + date +",'" + date1+"'+,'" + active+"' )";
+Integer m = st.executeUpdate(sql1);
+            if (m >= 1) {
+System.out.println("inserted successfully : " + sql1);
+} else {
+System.out.println("insertion failed");
+}
+            String sql2 = "Select * from Hotels";
+ResultSet resultSet = st.executeQuery(sql2);
+             while (resultSet.next()) {
+System.out.println(resultSet.getString("id"));
+System.out.println(resultSet.getString("name"));
+System.out.println(resultSet.getString("location"));
+System.out.println(resultSet.getString("date"));
+System.out.println(resultSet.getString("date1"));
+}
+            con.close();
+} catch (Exception ex) {
+System.err.println(ex);
+}
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Lenovo
+ *
+ */
+module Hotels {
+	requires java.sql;
+}
